6586e063e5c7503ec9325bef0dfa24bce906643d
Roy Tate <roytate@hotmail.com>
2019-10-08 20:42:58 -0700

    create function correctly saving data

--- main.py
+++ main.py
@@ -3,21 +3,38 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation, Donor
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return redirect(url_for('all'))
 
+
 @app.route('/donations/')
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+
+@app.route('/create/', methods=['POST', 'GET'])
+def create():
+    # if 'username' not in session:
+    #     return redirect(url_for('login'))
+    if request.method == 'POST':
+        donor_name = Donor.get_or_none(Donor.name == request.form['name'])
+        if donor_name is None:
+            donor_name = Donor(name=request.form['name'])
+            donor_name.save()
+        Donation(donor=donor_name, value=request.form['donation']).save()
+        return redirect(url_for('all'))
+
+    return render_template('create.jinja2')
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='localhost', port=port)
 
